8a40be384319ed2822601a6c233dc4110cc8e09f
Baiba Elste <baiba.elste@inbox.lv>
2023-04-18 20:56:12 +0300

    in progress

---
+++ test/main.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+use App\CurrencyConverter;
+
+require_once 'vendor/autoload.php';
+
+$amount = (float)readline('Enter amount: ');
+$currency = readline('Enter currency to convert: ');
+
+$converter = new CurrencyConverter();
+echo "EUR $amount -> {$converter->convertCurrency($amount, $currency)} $currency";
